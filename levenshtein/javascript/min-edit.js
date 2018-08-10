e696a899f7325e9e1032d82a6c6a245ed5f6bca3
alextanhongpin <alextan220990@gmail.com>
2018-08-10 22:48:19 +0800

    chore: add needleman-wunsch algorithm sample

--- levenshtein/javascript/min-edit.js
+++ levenshtein/javascript/min-edit.js
@@ -6,30 +6,37 @@ function main () {
   let n = s.length + 1
   let m = t.length + 1
 
-  let matrix = Array(m).fill(0).map(() => {
+  // Normally use `d` for distance
+  let d = Array(m).fill(0).map(() => {
     return Array(n).fill(0)
   })
 
+  // d(i,0) = i
   for (let i = 0; i <= n; i += 1) {
-    matrix[i][0] = i
+    d[i][0] = i
   }
 
+  // d(0,j) = j
   for (let j = 0; j <= m; j += 1) {
-    matrix[0][j] = j
+    d[0][j] = j
   }
 
-  console.log(matrix)
+  console.log(d)
 
   // https://people.cs.pitt.edu/~kirk/cs1501/Pruhs/Spring2006/assignments/editdistance/Levenshtein%20Distance.htm
 
   for (let i = 1; i <= n; i += 1) {
     let si = s[i - 1]
     for (let j = 1; j <= m; j += 1) {
-      let cost = si === t[j - 1] ? 0 : 1
-      matrix[i][j] = Math.min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost)
+      let cost = si === t[j - 1] ? 0 : 1 // Copy, else Substitution
+      d[i][j] = Math.min(
+	d[i - 1][j] + 1, // Deletion 
+	d[i][j - 1] + 1, // Insertion
+	d[i - 1][j - 1] + cost 
+      )
     }
   }
-  console.log(matrix, matrix[n][m])
+  console.log(d, d[n][m])
 }
 
 main()
---
+++ levenshtein/javascript/needleman-wunsch.js
@@ -0,0 +1,41 @@
+function main() {
+ 
+  let s = 'GATTACA' // Vertical 
+  let t = 'GCATGCU' // Horizontal
+
+  let n = s.length + 1 // Rows 
+  let m = t.length + 1 // Columns
+
+  // Normally use `d` for distance
+  let d = Array(n).fill(0).map(() => {
+    return Array(m).fill(0)
+  })
+
+  // d(i,0) = i
+  for (let i = 0; i < n; i += 1) {
+    d[i][0] = -i
+  }
+
+
+  // d(0,j) = j
+  for (let j = 0; j < m; j += 1) {
+    d[0][j] = -j
+  }
+
+  console.log(d)
+  for (let i = 1; i < n; i += 1) {
+    for (let j = 1; j < m; j += 1) {
+      let similarity = s[i - 1] === t[j - 1] ? 1 : -1
+      console.log('similarity', s[i], t[j], similarity)
+      d[i][j] = Math.max(
+        d[i - 1][j - 1] + similarity,
+        d[i][j - 1] - 1,
+        d[i - 1][j] - 1
+      )
+      console.log(d[i][j])
+    }
+  }
+  console.log(d, d[n - 1][m - 1])
+}
+
+main()
