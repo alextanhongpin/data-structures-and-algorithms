608cd42633649d7495eb1f1df8475e67c6066aa7
alextanhongpin <alex@policypal.com>
2018-10-10 11:14:10 +0800

    refactor(trie): improve code layout

--- trie/radix.py
+++ trie/radix.py
@@ -1,12 +1,11 @@
 #  https://en.wikipedia.org/wiki/Radix_tree
-from copy import deepcopy
 
 class Node():
     def __init__(self):
         self.edges = []
 
     def is_leaf(self):
-        return len(self.edges) == 0
+        return not self.edges
 
     def info(self, i = 0):
         for edge in self.edges:
@@ -24,71 +23,89 @@ class Edge():
         return '{}:{}'.format(self.label, self.score)
 
 def insert(root = None, word = ''):
-    if root is None or word is '': return
+    if not root and not word: return
 
-    # Prefix does not exist yet, add it.
+    # There are no edges. Create new.
     if root.is_leaf():
-        root.edges.append(Edge(word))
-        return
+        # Conventional to return None for mutating items in python.
+        return root.edges.append(Edge(word))
 
-    i = -1
-    idx = -1
-    next_edge = None
-    for idx, edge in enumerate(root.edges):
-        max_cmp = min(len(edge.label), len(word))
-        for k in range(max_cmp):
-            if edge.label[k] != word[k]:
-                continue
-            next_edge = edge
-            i = k + 1
+    next_edge = [(i, edge) for i, edge in enumerate(root.edges) 
+                 if similar_prefix(word, edge.label)] 
 
-    if next_edge is None or i == 0:
-        root.edges.append(Edge(word))
-        return
-    if next_edge.label == word:
+    if not next_edge:
+        return root.edges.append(Edge(word))
+        
+    i, next_edge = next_edge[0]
+    k = similar_prefix(word, next_edge.label)
+
+    if not k: 
+        # There are no matching prefix, which means that the word does not
+        # exist yet.
+        return root.edges.append(Edge(word))
+    if k == len(word):
+        # The len of both words matches. Increment the score as the same
+        # pattern has been found.
         next_edge.score += 1
         return
-    if next_edge.label in word:
+    if k == len(next_edge.label):
+        # The label is a prefix of the word. E.g, `car` is a prefix of `cars`.
+        # Insert the difference, which is `s` in the example above.
         next_edge.score += 1
         if next_edge.node is None:
             next_edge.node = Node()
-        insert(next_edge.node, word[len(next_edge.label):])
+        insert(next_edge.node, word[k:])
         return
 
-    edge = root.edges.pop(idx)
-    split_prefix = edge.label[:i]
-    edge.label = edge.label[i:]
+    # We need to split the prefix now since the length of k does not match any
+    # of them. A possible example is `john` and `jojo`, where k will be equal
+    # to 3, matching the prefix `jo` (john[:k] or jojo[:k]). In this scenario,
+    # we will add the new prefix `jo` and split the edges into `hn` and `jo`.
 
-    new_edge = Edge(split_prefix)
+    # Remove the old edge.
+    edge = root.edges.pop(i)
+
+    # Tmp variable to store the original label.
+    label = edge.label
+
+    # Update the existing label.
+    edge.label = label[k:]
+
+    # The new split prefix.
+    new_edge = Edge(label[:k])
     new_edge.node = Node()
     new_edge.score += edge.score
     new_edge.node.edges.append(edge)
 
-    insert(new_edge.node, word[i:])
+    insert(new_edge.node, word[k:])
     root.edges.append(new_edge)
 
-def lookup(root, word='', result = set()):
-    if root is None or word is '': return result
+def similar_prefix(a, b):
+    '''
+    Checks if string a and b has similar prefixes by comparing each characters.
+    If they do not have similar prefix, it should return index 0, indicating
+    that a[:index] or b[:index] is empty string. If they are perfect match, it
+    would return the exact length.
+    '''
+    values = zip(a, b)
+    for i, (left, right) in enumerate(values):
+        if left != right:
+            return i 
+    return len(values) 
+    
+def lookup(root, word='', result = None):
+    if result is None:
+        result = set()
 
     i = 0
     traverse_node = root
     while traverse_node is not None and not traverse_node.is_leaf() and i < len(word):
         next_edge = [edge for edge in traverse_node.edges
-                    if word[i:] in edge.label or
-                        edge.label in word[i:] 
-                    ]
-        print('found', [edge.label for edge in next_edge])
-        if len(next_edge) == 0:
+                     if word[i:].startswith(edge.label)]
+        if not next_edge:
             traverse_node = None
             break
-
         edge = next_edge[0]
-        #  if i + len(edge.label) > len(word):
-        #      print('exceeded length', i + len(edge.label))
-        #      #  i += len(edge.label)
-        #      #  traverse_node = edge.node
-        #      break
-        print('added', edge)
         traverse_node = edge.node
         i += len(edge.label)
 
@@ -98,13 +115,11 @@ def lookup(root, word='', result = set()):
     if i > len(word):
         return set() 
 
-    print('reach', i, word, word[:i], [edge.label for edge in traverse_node.edges])
     out = [word[:i] + edge.label for edge in traverse_node.edges]
     for o in out:
         result.add(o)
         lookup(root, o, result)
     return result
-
 
 def debug(root, word):
     insert(root, word)
@@ -113,6 +128,9 @@ def debug(root, word):
 
 root = Node()
 insert(root, 'a')
+insert(root, 'b')
+insert(root, 'bc')
+insert(root, 'cb')
 insert(root, 'ab')
 insert(root, 'ac')
 insert(root, 'abc')
