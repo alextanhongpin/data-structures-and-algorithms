db28ef624aa8553747527064e1b3f7f0ad5d76c5
alextanhongpin <alextan220990@gmail.com>
2018-10-15 23:21:00 +0800

    refactor(external-sort): optimize performance

--- external-sort/main.go
+++ external-sort/main.go
@@ -8,116 +8,126 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"runtime/pprof"
 )
 
 func main() {
-	n := 1000000
-	// writeSampleData(n)
-	// readSampleData(100)
-
-	// Number of items to sort.
-	buckets := 10
-	bucketSize := n / buckets
-
-	// Write CPU profile.
-	cpuf, err := os.Create("cpu.out")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pprof.StartCPUProfile(cpuf)
-	defer pprof.StopCPUProfile()
-
-	writeToTempFiles(buckets, bucketSize)
-
-	log.Println("finish writing to temp file")
-	mergeFiles()
-
-	memf, err := os.Create("mem.out")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pprof.WriteHeapProfile(memf)
-	defer memf.Close()
-}
-
-func writeSampleData(n int) {
-	f, err := os.Create("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	// rand.Seed(time.Now().UnixNano())
-
-	// Setup buffered writer.
-	w := bufio.NewWriterSize(f, 1024)
-	defer w.Flush()
-
-	for i := 0; i < n; i++ {
-		// Binary encoded integers is faster.
-		num := rand.Int63()
-		if err := binary.Write(w, binary.BigEndian, num); err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func readSampleData(limit int) {
-	f, err := os.Open("output.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	r := bufio.NewReader(f)
-	var count int
-	for {
-		var i int64
-		err := binary.Read(r, binary.BigEndian, &i)
-		if err != nil && err == io.EOF {
-			break
-		}
-		fmt.Println(i)
-		count++
-		if count > limit {
-			break
-		}
-	}
-	fmt.Println("read items", count)
-}
-
-func writeToTempFiles(buckets, bucketSize int) {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	var items []int64
-	var start, offset int
+	out, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// n := 1000000
+	// writeSampleData(f, n)
+
+	// head(out, 100)
+
+	// Number of items to sort.
+	// buckets := 10
+	// bucketSize := n / buckets
+	//
+	// // Profile CPU.
+	// cpuf, err := os.Create("cpu.out")
+	// if err != nil {
+	//         log.Fatal(err)
+	// }
+	// pprof.StartCPUProfile(cpuf)
+	// defer pprof.StopCPUProfile()
+	//
+	// // Actual logic.
+	// sortAndWriteFiles(f, buckets, bucketSize)
+	//
+	// mergeFiles(out, buckets)
+	//
+	// // Profile memory.
+	// memf, err := os.Create("mem.out")
+	// if err != nil {
+	//         log.Fatal(err)
+	// }
+	// pprof.WriteHeapProfile(memf)
+	// defer memf.Close()
+	// log.Println("main: completed, terminating program")
+}
+
+func writeSampleData(f *os.File, n int) {
+	w := bufio.NewWriterSize(f, 1024)
+	defer w.Flush()
+
+	for i := 0; i < n; i++ {
+		num := rand.Int63()
+		err := binary.Write(w, binary.BigEndian, num)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	log.Printf("writer: wrote %d integers to file\n", n)
+}
+
+func head(f *os.File, n int) {
+	r := bufio.NewReader(f)
+	var count int
+
+	var i int64
+	for {
+		err := binary.Read(r, binary.BigEndian, &i)
+		if err != nil && err == io.EOF {
+			break
+		}
+		fmt.Println(i)
+		count++
+		if count > n {
+			break
+		}
+	}
+}
+
+func sortAndWriteFiles(f *os.File, buckets, bucketSize int) {
+	// var items []int64
+	items := make([]int64, bucketSize)
+	var fileNum, offset int
+	var w *bufio.Writer
+	var i int64
 
 	r := bufio.NewReader(f)
 loop:
 	for {
-		var v int64
-		err := binary.Read(r, binary.BigEndian, &v)
+		// Read the integer from the file as int64 and append it to the
+		// slice in memory.
+		err := binary.Read(r, binary.BigEndian, &i)
+		if err != nil {
+			log.Println(err, i)
+		}
 		if err != nil && err == io.EOF {
 			break loop
 		}
-		items = append(items, v)
-		offset++
-		if offset == (start+1)*bucketSize {
+		items[offset%bucketSize] = i
+
+		if (offset+1)%bucketSize == 0 {
 			// Perform mergesort on the current bucket.
 			sort(items)
 
 			// Write to a temp file with the start index.
-			name := fmt.Sprintf("tmp_%d.txt", start)
+			name := fmt.Sprintf("tmp_%d.txt", fileNum)
 			f, err := os.Create(name)
 			if err != nil {
 				log.Fatal(err)
 			}
-			w := bufio.NewWriterSize(f, 1024)
+			if w == nil {
+				w = bufio.NewWriterSize(f, 1024)
+			} else {
+				// Reuse buffer.
+				w.Reset(f)
+			}
+
+			// Write the sorted items into a separate file.
 			for _, v := range items {
+				// for i := 0; i < bucketSize; i++ {
 				binary.Write(w, binary.BigEndian, v)
 			}
 			w.Flush()
@@ -125,100 +135,80 @@ loop:
 
 			// Reset the bucket.
 			items = items[:0]
+			items = items[:bucketSize]
 
 			// Increment the next bucket.
-			start++
+			fileNum++
+			fmt.Println("writer: completed", name)
 		}
+		// Increment the offset to keep track of the number of items.
+		offset++
 	}
 	fmt.Println("end scanning", offset)
 }
 
-func mergeFiles() {
-	n := 10
-
-	scanners := make([]*bufio.Reader, n)
+func mergeFiles(f *os.File, n int) {
+	// Initialize readers for the n-files we created earlier.
+	readers := make([]*bufio.Reader, n)
 	for i := 0; i < n; i++ {
 		f, err := os.Open(fmt.Sprintf("tmp_%d.txt", i))
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer f.Close()
-		scanners[i] = bufio.NewReader(f)
+		readers[i] = bufio.NewReader(f)
 	}
 
-	var items []int64
-	cache := make(map[int64]int, n)
-	counter := make(map[int]int, n)
+	items := make([]heapNode, n)
+	var v int64
 	for i := 0; i < n; i++ {
-		var v int64
-		binary.Read(scanners[i], binary.BigEndian, &v)
-		cache[v] = i
-		counter[i]++
-		items = append(items, v)
+		err := binary.Read(readers[i], binary.BigEndian, &v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Add items into the heap.
+		items[i] = heapNode{element: v, index: i}
 	}
 
-	outf, err := os.Create("output.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outf.Close()
-
-	w := bufio.NewWriterSize(outf, 1024)
+	w := bufio.NewWriterSize(f, 1024)
 	defer w.Flush()
 
+	var i int64
 	for len(items) > 0 {
 		minHeap(items)
-
-		min := items[0]
-		binary.Write(w, binary.BigEndian, min)
-
-		// Take the next item from the scanner.
-		items = extractMin(items)
-
-		idx := cache[min]
-		counter[idx]++
-
-		var v int64
-		err := binary.Read(scanners[idx], binary.BigEndian, &v)
+		err := binary.Write(w, binary.BigEndian, items[0].element)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Read the next value from the file that has previous min.
+		err = binary.Read(readers[items[0].index], binary.BigEndian, &i)
 		if err != nil && err == io.EOF {
-
-			delete(cache, min)
+			items = items[1:]
 			continue
 		}
-		items = append(items, v)
-		delete(cache, min)
-		cache[v] = idx
+		items[0].element = i
 	}
 }
 
-func extractMin(arr []int64) []int64 {
-	if len(arr) == 0 {
-		return arr
-	}
-
-	// Decrease heap size by 1.
-	n := len(arr) - 1
-	arr[0], arr = arr[n], arr[:n] // Truncate the slice.
-	if n > 0 {
-		heapify(arr, n, 0)
-	}
-	return arr
+type heapNode struct {
+	element int64
+	index   int
 }
 
-func minHeap(arr []int64) {
+func minHeap(arr []heapNode) {
 	n := len(arr)
 	for i := (n - 1) / 2; i > -1; i-- {
 		heapify(arr, n, i)
 	}
 }
 
-func heapify(arr []int64, n, i int) {
+func heapify(arr []heapNode, n, i int) {
 	smallest := i
 	l, r := 2*i+1, 2*i+2
-	if l < n && arr[l] < arr[smallest] {
+	if l < n && arr[l].element < arr[smallest].element {
 		smallest = l
 	}
-	if r < n && arr[r] < arr[smallest] {
+	if r < n && arr[r].element < arr[smallest].element {
 		smallest = r
 	}
 	if smallest != i {
@@ -228,9 +218,7 @@ func heapify(arr []int64, n, i int) {
 }
 
 func sort(arr []int64) {
-	// tmp := make([]int64, len(arr))
-	// copy(tmp, arr)
-	// mergesort(tmp, arr, 0, len(arr))
+	// Create a buffer to be reused.
 	buf := make([]int64, len(arr)/2)
 	mergesort(arr, buf)
 }
@@ -243,6 +231,8 @@ func mergesort(arr, buf []int64) {
 	left, right := arr[:mid], arr[mid:]
 	mergesort(left, buf)
 	mergesort(right, buf)
+
+	// We can sort 2 n-size array with an additional n-size array.
 	copy(buf, left)
 	l, r := 0, 0
 	for l < len(left) && r < len(right) {
@@ -259,7 +249,7 @@ func mergesort(arr, buf []int64) {
 		l++
 	}
 	for r < len(right) {
-		arr[r+l] = right[r]
+		arr[l+r] = right[r]
 		r++
 	}
 }
