d76dbccdc15c7bb0d588862601512d94595710a1
Alex Tan Hong Pin <alextan220990@gmail.com>
2018-10-17 15:14:40 +0800

    docs(levenshtein): cleanup go example

--- levenshtein/go/levenshtein.go
+++ levenshtein/go/levenshtein.go
@@ -2,138 +2,116 @@ package main
 
 import (
 	"fmt"
-	"strings"
-)
-
-var (
-	DeleteCost     = 1
-	InsertCost     = 1
-	SubstituteCost = 1
 )
 
 const (
-	DELETE int = iota
-	INSERT
-	REPLACE
+	Replace int = iota
+	Delete
+	Insert
+)
+
+var (
+	ReplaceCost = 1
+	DeleteCost  = 1
+	InsertCost  = 1
 )
 
 func main() {
-	var (
-		source = []rune("places")
-		target = []rune("palace")
-	)
-	l := levenshtein(source, target)
-	fmt.Println(l)
-	longest := max(len(source), len(target))
+	src, tgt := []byte("sitting"), []byte("kitten")
 
-	score := (float64(1) - float64(l)/float64(longest)) * 100
-	fmt.Println(score)
+	cost, ops := levenshtein(src, tgt)
+	fmt.Println("cost is", cost)
+	fmt.Println("start:", string(src))
+	for i, op := range ops {
+		fmt.Println("Step", i+1, op)
+	}
+	fmt.Println("end:", string(tgt))
 }
 
-func levenshtein(source, target []rune) int {
-	s, t := len(source)+1, len(target)+1
+func levenshtein(src, tgt []byte) (int, []string) {
+	s, t := len(src)+1, len(tgt)+1
 
-	table := make([][]int, s)
+	m := make([][]int, s)
 	op := make([][]int, s)
 
-	for row := range table {
-		table[row] = make([]int, t)
-		op[row] = make([]int, t)
-		table[row][0] = row
-	}
-	for col := range table[0] {
-		table[0][col] = col
-	}
+	for i := 0; i < s; i++ {
+		m[i] = make([]int, t)
+		m[i][0] = i
 
-	indicatorFunction := func(a, b rune) int {
-		if a == b {
-			return 0
-		}
-		return SubstituteCost
+		op[i] = make([]int, t)
+	}
+	for j := 0; j < t; j++ {
+		m[0][j] = j
 	}
 
 	for i := 1; i < s; i++ {
 		for j := 1; j < t; j++ {
-			costs := []int{
-				table[i][j-1] + InsertCost,                                    // Insertion
-				table[i-1][j] + DeleteCost,                                    // Deletion
-				table[i-1][j-1] + indicatorFunction(source[i-1], target[j-1]), // Substitution
+			replaceCost := func() int {
+				indicator := m[i-1][j-1]
+				if src[i-1] == tgt[j-1] {
+					return indicator + 0
+				}
+				return indicator + ReplaceCost
 			}
-			cost := min(costs...)
-			table[i][j] = cost
-			op[i][j] = indexOf(cost, costs)
+			deleteCost := m[i-1][j] + DeleteCost
+			insertCost := m[i][j-1] + InsertCost
+
+			costs := []int{replaceCost(), deleteCost, insertCost}
+			m[i][j] = min(costs...)
+
+			op[i][j] = index(costs, m[i][j])
 		}
 	}
-
-	// Walk the path backward.
-	i, j := len(source), len(target)
-
 	var ops []string
+	i, j := len(src), len(tgt)
 	for i != 0 && j != 0 {
 		cost := op[i][j]
 		switch {
-		case cost == DELETE || j == 0:
-			str := fmt.Sprintf("deleted %d-th string (%c) for %s", i, source[i-1], string(source))
-			ops = append(ops, str)
+		case cost == Replace:
+			// b = append(b[:i], append([]byte("\u0336"), b[i:]...)...)
 			i--
-		case cost == INSERT || i == 0:
-			str := fmt.Sprintf("insert %d-th string (%c) for %s", j, target[j-1], string(target))
-			ops = append(ops, str)
 			j--
-		case cost == REPLACE:
-			if table[i-1][j-1] < table[i][j] {
-				str := fmt.Sprintf("replaced %d-th string (%c) with %c, %s", j, source[i-1], target[j-1], string(target))
-				ops = append(ops, str)
+			// If the scores are the same, it means there are no change.
+			if m[i][j] < m[i+1][j+1] {
+				s := fmt.Sprintf("replace %d-th character %c into %c", i+1, src[i], tgt[j])
+				ops = append(ops, s)
 			}
+		case cost == Delete || j == 0:
 			i--
+			s := fmt.Sprintf("delete %d-th character %c", i+1, src[i])
+			ops = append(ops, s)
+		case cost == Insert || i == 0:
 			j--
+			s := fmt.Sprintf("insert %d-th character %c", j+1, tgt[j])
+			ops = append(ops, s)
+
 		}
 	}
-	fmt.Println(strings.Join(reverseString(ops), "\n"))
-	return table[s-1][t-1]
+	reverse(ops)
+	return m[s-1][t-1], ops
+}
+func reverse(arr []string) {
+	for i := 0; i < len(arr)/2; i++ {
+		j := len(arr) - i - 1
+		arr[i], arr[j] = arr[j], arr[i]
+	}
 }
 
-func min(in ...int) int {
-	out := 1<<7 - 1
-	for _, i := range in {
-		if i < out {
-			out = i
+func min(nums ...int) int {
+	val := 1<<7 - 1
+	for _, i := range nums {
+
+		if i < val {
+			val = i
 		}
 	}
-	return out
+	return val
 }
 
-func max(in ...int) int {
-	out := -1 << 7
-	for _, i := range in {
-		if i > out {
-			out = i
-		}
-	}
-	return out
-}
-
-func reverse(in []rune) []rune {
-	for i := 0; i < len(in)/2; i++ {
-		j := len(in) - 1 - i
-		in[i], in[j] = in[j], in[i]
-	}
-	return in
-}
-
-func reverseString(in []string) []string {
-	for i := 0; i < len(in)/2; i++ {
-		j := len(in) - 1 - i
-		in[i], in[j] = in[j], in[i]
-	}
-	return in
-}
-
-func indexOf(i int, in []int) int {
-	// We traverse backwards, indicating that we prefer substitution.
-	for j := len(in) - 1; j > -1; j-- {
-		if in[j] == i {
-			return j
+func index(arr []int, n int) int {
+	for i, v := range arr {
+		if v == n {
+			return i
 		}
 	}
 	return -1
